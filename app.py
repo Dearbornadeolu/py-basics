edd346abd74056fa3f7953a71b2f3bd4baa59e55
Dearborn Adeyeni <dearbornadeyeni@gmail.com>
2023-10-03 20:08:12 +0100

    inro to python

---
+++ app.py
@@ -0,0 +1,44 @@
+print("hello world")
+# String Values
+name = "Dearborn is a boy"
+print(name)
+
+# Integars or number
+number = 4
+print(number)
+
+# list values
+contries = ["nigeria", "south Africa", "Ghana", "kenya"]
+print(contries)
+ # using index
+print(contries[0])
+# pop out the arrays
+contries.pop(1)
+print(contries)
+
+# another type of list 
+states = list(("lagos", "osun", 3, "ogun"))
+print(states)
+
+# delete states
+del states
+# print(states)
+
+# Addition of two numbers
+
+number1 = 2367
+number2 = 5676237
+total = number1 + number2 + number
+print(total)
+
+# concatintion
+founder = "Dearborn"
+coFounder = "Tade"
+coFounder2 = "Sharon"
+rest = " are founders and co-founders"
+print(founder + " "+ coFounder+ " " + coFounder2 + rest)
+print(founder + " "+ coFounder+ " " + coFounder2 + rest + "with", 5, "years")
+
+# Tuple
+number_zero = ("next",)
+print(type(number_zero))
